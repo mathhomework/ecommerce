c543d6630cb878aeaeeff8f479f7c9c0a074084b
mathhomework <toxstalks@gmail.com>
2014-10-16 18:16:11 -0700

    first commit

---
+++ local_settings.py
@@ -0,0 +1,8 @@
+DATABASES={
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'hotdog'
+    }
+}
